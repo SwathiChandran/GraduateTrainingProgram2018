b2d0acbea68b1d4746cfc177686ae85b14835321
SwathiChandran <41384692+SwathiChandran@users.noreply.github.com>
2018-09-18 12:43:16 +0530

    Update Day3.py

--- python/Day3.py
+++ python/Day3.py
@@ -1,34 +1,70 @@
-
+"""
 inventory = {
                    'gold' : 500,
                    'pouch' : ['flint', 'twine', 'gemstone'],
                    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
                 }
-inventory['pocket']=['seashell', 'strange berry','lint']
-inventory['backpack'].sort()
-print("sort of backpack %s" %inventory['backpack'])
-inventory['backpack'].remove('dagger')
-print("sort of backpack %s" %inventory['backpack'])
-inventory['gold']+=50
-print(" gold list %s" %inventory['gold'])
 
-d={}
-i=raw_input("Enter no of students ")
-x=0
-while x<int(i):
-	s=raw_input("enter the name ")
-	r=raw_input("enter no of subjects ")
-	d[s]=[]
-	f=0
-	while f<int(r):
-		t=raw_input("enter the subject marks ")
-		d[s].append(t)
-		f+=1
-	x+=1
-print(d)
-for k in d.keys():
-	l=sum(d[k])
-	h=l/r
-	print("student name %s" %d[k])
-	print("sum %s" %str(l))
-	print("avg %s" %str(h))
+
+
+inventory ["pocket"]=["seashell","strange berry","lint"]
+print(inventory)
+inventory["backpack"].sort()
+print(inventory["backpack"])
+inventory["backpack"].remove("dagger")
+print(inventory["backpack"])
+inventory["gold"]+=50
+print inventory
+"""
+"""
+def func():
+	Student={}
+	Student["student1"]=[90,85,89]
+	Student["student2"]=[78,67,39]
+	print(Student)
+	total1=sum(Student["student1"])
+	total2=sum(Student["student2"])
+	print("total1 %s"%total1)
+	print("total2 %s"%total2)
+	avg1=total1/3
+	avg2=total2/3
+	print("avg1 %s"%avg1)
+	print("avg2 %s"%avg2)
+func()
+"""
+"""
+stud={}
+stud_count=input("enter no of students")
+for i in range(stud_count):
+ name=raw_input("enter the name")
+ sub_count=input("enter sub count")
+ m=[]
+ for j in range(sub_count):
+   j=j+1
+   marks=input("enter marks of sub%s" %j)
+   m.append(marks)
+   
+ stud[name]=m
+print(stud)
+"""
+"""
+try:
+ print(1/0)
+except Exception as error:
+ print(error)
+finally:
+ print("finally block")
+ """
+ """
+def add_func(a,b):
+  try:
+   c=a+b
+   print(c)
+  except Exception as error:
+   print(error)
+  finally:
+   print("finally block")
+add_func(5,5)     
+ """
+ 
+ 
